15aed6e96a8c4ecc93c60d690577d854c6d191b8
Brian Thomas Smith <brian@smith.berlin>
2023-02-24 12:48:45 +0100

    fix: replace babel-eslint with @babel/eslint-parser (#123)
    * Migrate from Yarn to NPM
    
    * fix(serve): babel-eslint is now @babel/eslint-parser
    
    ---------
    
    Co-authored-by: Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>

--- src/Home.vue
+++ src/Home.vue
@@ -1,112 +1,111 @@
 <template>
-  <header>
-    <h1>MDN Content Tools</h1>
-  </header>
-  
-  <section class="page-home">
-    <a href="#/releases">Releases</a>
-    <a href="#/github">GitHub</a>
-  </section>
+  <div>
+    <header>
+      <h1>MDN Content Tools</h1>
+    </header>
+
+    <section class="page-home">
+      <a href="#/releases">Releases</a>
+      <a href="#/github">GitHub</a>
+    </section>
+  </div>
 </template>
 
 <script>
-
 export default {
-  name: "HomePage"
+  name: "HomePage",
 };
 </script>
 
 <style>
-  /** creating some base styles */
-  :root {
-    --bkColour:  #fefefe;
-    --fontColour: #222;
-    --highlightColour: tomato;
-    --spacing: 2rem;
-  }
+/** creating some base styles */
+:root {
+  --bkColour: #fefefe;
+  --fontColour: #222;
+  --highlightColour: tomato;
+  --spacing: 2rem;
+}
 
-  html, body {
-    margin: 0px; padding: 0px;
-  }
-  body {
-    background-color: var(--bkColour);
-    font-family: Avenir, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    color:  var(--fontColour);
-    font-size: 100%;
-  }
+html,
+body {
+  margin: 0px;
+  padding: 0px;
+}
+body {
+  background-color: var(--bkColour);
+  font-family: Avenir, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: var(--fontColour);
+  font-size: 100%;
+}
 
-  h1, h2, h3 {
-    margin-bottom: var(--spacing);
-  }
+h1,
+h2,
+h3 {
+  margin-bottom: var(--spacing);
+}
 
-  p {
-    font-size: calc(var(--spacing)/0.6);
-  }
+p {
+  font-size: calc(var(--spacing) / 0.6);
+}
 
-  label, input, button, select {
-    padding: 4px 8px;
-    font-family: Avenir, sans-serif;
-    font-size: 100%;
-  }
+label,
+input,
+button,
+select {
+  padding: 4px 8px;
+  font-family: Avenir, sans-serif;
+  font-size: 100%;
+}
 
-  code {
-    font-size: 120%;
-  }
+code {
+  font-size: 120%;
+}
 
-  button {
-    background-color: var(--bkColour);
-    border: 4px solid var(--highlightColour);
-    color: var(--highlightColour);
-    font-weight: bold;
-  }
-  button:hover {
-    background-color: var(--highlightColour);
-    color:  var(--bkColour);
-    cursor: pointer;
-  }
+button {
+  background-color: var(--bkColour);
+  border: 4px solid var(--highlightColour);
+  color: var(--highlightColour);
+  font-weight: bold;
+}
+button:hover {
+  background-color: var(--highlightColour);
+  color: var(--bkColour);
+  cursor: pointer;
+}
 
-  a {
-    color: var(--highlightColour);
-    text-decoration: underline;
-  }
-  a:hover {
-    text-decoration: none;
-  }
+a {
+  color: var(--highlightColour);
+  text-decoration: underline;
+}
+a:hover {
+  text-decoration: none;
+}
 
-  .app-container {
-    margin: var(--spacing);
-  }
-
+.app-container {
+  margin: var(--spacing);
+}
 </style>
 
 <style scoped>
-  .page-home {}
-  .page-home a {
-    display:  block;
-    padding:  var(--spacing);
-    margin: var(--spacing);
+.page-home {
+}
+.page-home a {
+  display: block;
+  padding: var(--spacing);
+  margin: var(--spacing);
 
-    border:  2px solid var(--fontColour);
-    border-radius: 20px;
-    box-shadow:
-      inset 2em 0.5em #ddd,
-      0.3em 0.3em var(--highlightColour)
-    ;
+  border: 2px solid var(--fontColour);
+  border-radius: 20px;
+  box-shadow: inset 2em 0.5em #ddd, 0.3em 0.3em var(--highlightColour);
 
-    text-align: center;
-    text-decoration: none;
-    font-size: 4rem;
-  }
-  .page-home a:hover {
-    transform: translate(10px, 10px);
-    box-shadow:
-      inset 2em 0.5em #ddd,
-      0.2em 0.2em var(--highlightColour)
-    ;
-  }
+  text-align: center;
+  text-decoration: none;
+  font-size: 4rem;
+}
+.page-home a:hover {
+  transform: translate(10px, 10px);
+  box-shadow: inset 2em 0.5em #ddd, 0.2em 0.2em var(--highlightColour);
+}
 </style>
-
-
-
